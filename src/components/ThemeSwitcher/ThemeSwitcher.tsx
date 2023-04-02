19a4e9c383bedfdc1f75909dce1b68a1fbda1480
Bereke Kabdi <kabdibereke@gmail.com>
2023-04-02 22:11:52 +0600

    fix theme

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -6,18 +6,36 @@ import Image from "next/image"
 
 
 const ThemeSwitcher = () => {
+    //@ts-ignore
     const [isDark, setIsDark] =useState(false)
     const themeIcon = isDark? 'dark': 'light'
+   
+    useEffect(()=> {
+      if(localStorage.getItem('theme')===null) {
+        localStorage.setItem('theme', JSON.stringify(isDark))
+      }
+      if(localStorage.getItem('theme')!==null) {
+        //@ts-ignore
+        setIsDark(JSON.parse(localStorage.getItem('theme')))
+      }
+    },[])
+
     useEffect(()=> {
         document.body.setAttribute('data-theme' , isDark? 'dark': 'light')
     }, [isDark])
+
+    const themeSwitcher =()=> {
+      setIsDark(!isDark)
+      localStorage.setItem('theme', JSON.stringify(!isDark))
+    }
   return (
     <div className={styles.toggle} >
          <Image src={sun} alt="dsf" />
          <button
         className={styles.toggleButton}
         data-active-theme={themeIcon}
-        onClick={()=> setIsDark(!isDark)}
+        
+        onClick={themeSwitcher}
         ></button>
           <Image src={moon} alt="dsf" />
        
